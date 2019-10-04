46c252337edffe7e2e13f1617a592a8c988121c1
Mike Katsupko <t63723@gmail.com>
2019-10-04 11:32:09 +0700

    add media settings

--- basis/settings.py
+++ basis/settings.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Asia/Novosibirsk'
 
@@ -120,3 +120,7 @@ if DEBUG:
     )
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
